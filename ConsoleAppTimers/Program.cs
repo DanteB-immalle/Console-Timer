2ea0c4651dc5f88e751486b714df9299f4d09dff
Dante Budin <dante.budin@immalle.eu>
2017-12-01 11:12:43 +0100

    ConsoleTmer

--- ConsoleAppTimers/Program.cs
+++ ConsoleAppTimers/Program.cs
@@ -7,8 +7,39 @@ namespace ConsoleAppTimers
 {
     class Program
     {
+        static int globalCounter = 0;
+
         static void Main(string[] args)
         {
+            System.Timers.Timer tmr = new System.Timers.Timer();
+
+           
+            tmr.Interval = 200;
+            tmr.Start();
+
+            int counter = 0;
+
+         
+            tmr.Elapsed += (sender, e) =>
+            {
+                Console.WriteLine("Timer elapsed... {0}", counter);
+                counter++;
+            };
+
+            tmr.Elapsed += Tmr_Elapsed;
+
+            Console.ReadLine();
+        }
+
+        private static void Tmr_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("Timer elapsed 2... {0}", globalCounter);
+            globalCounter++;
+        }
+
+        private static void Tmr_Disposed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Timer disposed...");
         }
     }
 }
